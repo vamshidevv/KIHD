9fceec9ea56c1db3b791ea528d6de1d73ddb6005
vamshidevv <vamshi.bhandari.5057@gmail.com>
2024-06-24 11:08:19 +0530

    added navbar to dashboard

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import React from "react";
 
 export const DashBoard = () => {
   return (
-    <div>
-
-        <h2>Hello Iam from dashboard</h2>
-    </div>
-  )
-}
+    <>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "#2e5c9e",
+          height: "80px",
+          lineHeight: "80px",
+          boxShadow: "none",
+          paddingLeft: "30px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Container maxWidth="l">
+          <Toolbar disableGutters>
+            {/* <Box
+            component="img"
+            sx={{
+              display: { xs: "none", md: "flex" },
+              mr: 1,
+              height: 80,
+            //   width: 191,
+            }}
+            alt="Logo"
+            src={koli}
+          /> */}
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              id="logo-name"
+              sx={{ lineHeight: "85px" }}
+            >
+              KOLI INFOETCH
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
+  );
+};
